bc8504701358cb86172f622978fb437a7d77ab94
willem Sebastian <willem1991@yahoo.co.id>
2020-02-23 19:37:25 +0700

    Updates

---
+++ precache-manifest.ba217e0bd54921a0618aebb6c2877bda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa9313c913bc7968154a1078b4aad23c",
+    "url": "/Fabelio.github.io/index.html"
+  },
+  {
+    "revision": "9156a935a99cf197fc93",
+    "url": "/Fabelio.github.io/static/css/main.160a31a7.chunk.css"
+  },
+  {
+    "revision": "3c8d90fbecd61e1512d8",
+    "url": "/Fabelio.github.io/static/js/2.9d5c5a17.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Fabelio.github.io/static/js/2.9d5c5a17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9156a935a99cf197fc93",
+    "url": "/Fabelio.github.io/static/js/main.6c6efaba.chunk.js"
+  },
+  {
+    "revision": "c5be69edac0d93f7d73c",
+    "url": "/Fabelio.github.io/static/js/runtime-main.05f9e5af.js"
+  }
+]);
